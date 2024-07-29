d8fc7c3c528e4d417969b8bcde287edf9fda7afe
Nextoore <anex831@gmail.com>
2024-07-29 23:25:24 +0300

    защиточка

--- ds/src/routes/MainPage/main.tsx
+++ ds/src/routes/MainPage/main.tsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <main class={styles.wrapper}>
       <div class={styles.header}></div>
+      <form action="">
+        <div class={styles.container}>
+          <input type="text" />
+          <button>Send</button>
+        </div>
+      </form>
     </main>
   );
 }
--- ds/src/routes/index.tsx
+++ ds/src/routes/index.tsx
@@ -7,12 +7,20 @@ import { postToServer } from "~/api";
 import sai from './radio.module.scss'
 import styles from '../App.module.scss'
 import img from '../img/image.png'
-
+import { createSignal } from "solid-js";
 
 
 export default function Login(props: RouteSectionProps) {
   const loggingIn = useSubmission(postToServer);
-  
+  const [count, setCount] = createSignal(0);
+  const handler = () => {
+    setCount((prev) => prev + 1)
+    console.log(count())   
+  }
+  function safety(){
+    setCount((prev) => prev = 0) 
+  }
+  setTimeout(safety, 60000);
   return (
     <main class={styles.wrapper}>
         <img src={img}></img>
@@ -37,10 +45,15 @@ export default function Login(props: RouteSectionProps) {
             </div>
           </div>
         
-          <button type="submit" class={styles.btn}>Login</button> 
+          <button type="submit" class={styles.btn} onClick={[handler]} disabled={count() > 3}>Login</button> 
           <Show when={loggingIn.result} >
             <p class={styles.alert} role="alert" id="error-message">
               {loggingIn.result!.message}
+            </p>
+          </Show>
+          <Show when={count() > 3} >
+            <p class={styles.alert} role="alert" id="error-message">
+            Привышен лимит запросов, пожалуйста, подождите
             </p>
           </Show>
         </form>
