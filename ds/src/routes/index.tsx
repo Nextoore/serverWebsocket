856735e35612d2685c925693dcb7c3b5ff4121ad
Nextoore <anex831@gmail.com>
2024-07-29 19:23:23 +0300

    некстура в деле

--- ds/src/routes/index.tsx
+++ ds/src/routes/index.tsx
@@ -1,9 +1,49 @@
-import { createAsync, type RouteDefinition } from "@solidjs/router";
+import {
+  useSubmission,
+  type RouteSectionProps
+} from "@solidjs/router";
+import { Show } from "solid-js";
+import { postToServer } from "~/api";
+import sai from './radio.module.scss'
+import styles from '../App.module.scss'
+import img from '../img/image.png'
 
-export default function Home() {
+
+
+export default function Login(props: RouteSectionProps) {
+  const loggingIn = useSubmission(postToServer);
   
   return (
-    <main>
+    <main class={styles.wrapper}>
+        <img src={img}></img>
+        <form action={postToServer} method="post" class={styles.form}>
+          <input type="hidden" name="redirectTo" value={props.params.redirectTo ?? "/"} />
+          <fieldset>
+            <label class={sai.particlescheckboxcontainer}>
+              <input type="radio" name="loginType" value="login" checked={true} class={sai.particlescheckbox}/> 
+              <span><h1>Login</h1></span>
+            </label>
+            <label class={sai.particlescheckboxcontainer}>
+              <input type="radio" name="loginType" value="register" class={sai.particlescheckbox}/>
+              <span><h1>Register</h1></span>
+            </label>
+          </fieldset>
+          <div class={styles.container}>
+            <div class={styles.inputik}>
+              <input name="username" placeholder="Login" autocomplete="username" />
+            </div>
+            <div class={styles.inputik}>
+              <input name="password" type="password" placeholder="Password" autocomplete="current-password" />
+            </div>
+          </div>
+        
+          <button type="submit" class={styles.btn}>Login</button> 
+          <Show when={loggingIn.result} >
+            <p class={styles.alert} role="alert" id="error-message">
+              {loggingIn.result!.message}
+            </p>
+          </Show>
+        </form>
       
     </main>
   );
---
+++ ds/src/routes/main.tsx
@@ -0,0 +1,10 @@
+import { createAsync, type RouteDefinition } from "@solidjs/router";
+
+export default function Home() {
+  
+  return (
+    <main>
+      <h1>hiii</h1>
+    </main>
+  );
+}
