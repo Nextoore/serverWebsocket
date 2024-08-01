78349308984f2145ebbb36183f3715be86cb287f
Arthur730 <134970703+Arthur730@users.noreply.github.com>
2024-08-02 02:52:10 +0300

    Update main.tsx

--- ds/src/routes/MainPage/main.tsx
+++ ds/src/routes/MainPage/main.tsx
@@ -1,10 +1,12 @@
 import { createSignal, createEffect } from "solid-js"; 
 import s from './main.module.scss'
 import { useLocation } from "@solidjs/router";
-let socket: WebSocket | undefined; 
+
+let socket: WebSocket | undefined;
+
 const SERVER_IP = '185.102.139.56';
 const SERVER_PORT = 9999;
-const SERVER_URL = `ws://${SERVER_IP}:${SERVER_PORT}`
+const SERVER_URL = `ws://${SERVER_IP}:${SERVER_PORT}`;
 
 export default function Home() { 
   const [message, setMessage] = createSignal<{ chat: string; messages: any[] }[]>([]); 
@@ -14,7 +16,6 @@ export default function Home() {
   const [username, setUsername] = createSignal<string | undefined>(undefined);
 
   const location = useLocation();
-
   const queryParams = new URLSearchParams(location.search);
   const usernameParam = queryParams.get("username");
 
@@ -22,8 +23,6 @@ export default function Home() {
     setUsername(usernameParam);
   }
 
-
-  console.log(2)
   function connection() { 
     if (chatRoom() === '') {
       console.log('Chat room is required.');
@@ -37,7 +36,7 @@ export default function Home() {
       setReady(true); 
       const msg = { 
         event: 'connection', 
-        user: user(), 
+        user: username(), 
         chat: chatRoom(), 
         id: Date.now() 
       }; 
@@ -45,16 +44,21 @@ export default function Home() {
     }; 
 
     socket.onmessage = (event) => { 
-      const receivedMessage = JSON.parse(event.data); 
-      setMessage((prevArray) => {
-        const existingChat = prevArray.find(chat => chat.chat === receivedMessage.chat);
-        if (existingChat) {
-          existingChat.messages.push(receivedMessage);
-          return [...prevArray.filter(chat => chat.chat !== receivedMessage.chat), existingChat];
-        } else {
-          return [...prevArray, { chat: receivedMessage.chat, messages: [receivedMessage] }];
-        }
-      });
+      const receivedMessage = JSON.parse(event.data);
+
+      if (receivedMessage.event === 'history') {
+        setMessage([{ chat: receivedMessage.chat, messages: receivedMessage.messages }]);
+      } else {
+        setMessage((prevArray) => {
+          const existingChat = prevArray.find(chat => chat.chat === receivedMessage.chat);
+          if (existingChat) {
+            existingChat.messages.push(receivedMessage);
+            return [...prevArray.filter(chat => chat.chat !== receivedMessage.chat), existingChat];
+          } else {
+            return [...prevArray, { chat: receivedMessage.chat, messages: [receivedMessage] }];
+          }
+        });
+      }
     }; 
 
     socket.onerror = (error) => { 
@@ -70,7 +74,7 @@ export default function Home() {
   function sendMessage() { 
     if (socket && socket.readyState === WebSocket.OPEN && chatRoom() !== '') { 
       const msg = { 
-        user: user(), 
+        user: username(), 
         message: user(), 
         id: Date.now(), 
         event: 'message',
@@ -99,26 +103,16 @@ export default function Home() {
     <main>
         {!ready() ? ( 
           <div>
-            <p>{username()}</p>
             <input 
               type="text" 
               placeholder='Chatname' 
               value={chatRoom()} 
               onInput={(e) => setChatRoom(e.target.value)} 
             /> 
-            <input 
-              type="text" 
-              placeholder='Enter name' 
-              value={user()} 
-              onInput={(e) => setUser(e.target.value)} 
-            /> 
             <button onClick={connection} class={s.btn}>Join Chat Room</button> 
           </div>
         ) : ( 
           <div>
-            <div>
-              
-            </div>
             <div>
               <input 
                 type="text" 
@@ -135,8 +129,8 @@ export default function Home() {
                   {chat.messages.map((mess) => (
                     <div key={mess.id}>
                       {mess.event === 'connection'
-                        ? <div> {mess.user} joined </div>
-                        : <div> {mess.message} </div>
+                        ? <div> {mess.sender} joined </div>
+                        : <div> {mess.sender}: {mess.message} </div>
                       }
                     </div>
                   ))}
@@ -147,6 +141,7 @@ export default function Home() {
     </main> 
   ); 
 }
+
 
 
 
