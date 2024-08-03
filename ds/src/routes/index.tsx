a71c2d7cfec0249239114726706da8312de88fe9
Nextoore <anex831@gmail.com>
2024-08-03 04:38:06 +0300

    Кнопочки-кнопочки

--- ds/src/routes/index.tsx
+++ ds/src/routes/index.tsx
@@ -6,7 +6,10 @@ import { Show } from "solid-js";
 import { postToServer } from "~/api";
 import sai from './radio.module.scss'
 import styles from '../App.module.scss'
-import img from './next.module.scss'
+import img from '../img/image.png'
+import lock from '../icons/lock-alt-solid-24.png'
+import mail from '../icons/envelope-solid-24.png'
+import user from '../icons/user-solid-24.png'
 import { createSignal } from "solid-js";
 
 
@@ -44,6 +47,7 @@ export default function Login(props: RouteSectionProps) {
   }
   return (
     <main class={styles.wrapper}>
+      <img src={img}></img>
       <div class={styles.home} classList={{[styles.nexto]: clas()}}>
         <div class={styles.loogin} classList={{[styles.next]: clas()}}>
             <form action={postToServer} method="post" class={styles.form}>
@@ -52,12 +56,18 @@ export default function Login(props: RouteSectionProps) {
               <input type="radio" name="loginType" checked={true} value="login" class={sai.particlescheckbox}/>
                 <div class={sai.div}><h1>Login</h1></div>
 
-              <div class={styles.inputik}>
-                <input name="username" placeholder="Login" autocomplete="username" />
-              </div>
-              <div class={styles.inputik}>
-                <input name="password" type="password" placeholder="Password" autocomplete="current-password" />
-              </div>
+                <div class={styles.inputik}>
+                  <span>
+                    <img src={user}/>
+                  </span>
+                  <input name="username" placeholder="Username" autocomplete="nickname" />
+                </div>
+                <div class={styles.inputik}>
+                  <span>
+                    <img src={lock}/>
+                  </span>
+                  <input name="password" type='password' placeholder="Password" autocomplete="current-passwordnew-password" />
+                </div> 
 
             <button type="submit" class={styles.btn} onClick={handler} disabled={count() > 3}>Login</button> 
             <Show when={loggingIn.result} >
@@ -81,24 +91,27 @@ export default function Login(props: RouteSectionProps) {
           <div class={sai.div}><h1>Register</h1></div >
 
             <div class={styles.inputik}>
-              <input name="username" placeholder="Login" autocomplete="username" />
-            </div>
+              <span>
+                <img src={user}/>
+              </span>
+              <input name="username" placeholder="Username" autocomplete="nickname" />
+            </div> 
             <div class={styles.inputik}>
-              <input name="mail" type="mail" placeholder="Mail" autocomplete="current-password" />
-            </div>
+              <span>
+                <img src={mail}/>
+              </span>
+              <input name="mail" type="email" placeholder="Mail" autocomplete="email" />
+            </div> 
             <div class={styles.inputik}>
-              <input name="password" type="password" placeholder="Password" autocomplete="current-password" />
-            </div>
-                    
+              <span>
+                <img src={lock}/>
+              </span>
+              <input name="password" type='password' placeholder="Password" autocomplete="current-password" />
+            </div>                   
           <button type="submit" class={styles.btn} onClick={handler} disabled={count() > 3}>Registration</button> 
           <Show when={loggingIn.result} >
             <p class={styles.alert} role="alert" id="error-message">
               {loggingIn.result!.message}
-            </p>
-          </Show>
-          <Show when={count() > 3} >
-            <p class={styles.alert} role="alert" id="error-message">
-            Привышен лимит запросов, пожалуйста, подождите
             </p>
           </Show>
         </form>
